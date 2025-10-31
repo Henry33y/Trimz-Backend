9f46081fac735e6ec5802dfec8365771020abf9e
Henry <henryasante336@gmail.com>
2025-10-31 08:30:17 +0000

    update Google OAuth callback URL to use environment variable

--- config/passport.config.js
+++ config/passport.config.js
@@ -13,7 +13,7 @@ passport.use(new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:5000/api/auth/google/callback',
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
     passReqToCallback: true,
   },
   async (request, accessToken, refreshToken, profile, done) => {
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,11 @@ import { Router } from "express";
 import jwt from 'jsonwebtoken'
 import passport from "passport";
 import { forgotPasswordController, login, logout, resetPasswordController } from "../controllers/auth.controller.js";
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+const frontend_URL = process.env.FRONTEND_URL
 const loginRouter = Router()
 
 //Login and logout routes
@@ -13,13 +17,13 @@ loginRouter.post("/auth/reset-password/:token", resetPasswordController)
 // Google OAuth2 login
 loginRouter.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'], prompt: 'select_account consent' }));
 // Google OAuth2 callback
-loginRouter.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: 'http://localhost:5173/register', session: false }), (req, res) => {
+loginRouter.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: `${frontend_URL}`, session: false }), (req, res) => {
   const { token } = req.user;
   const { role } = req.user.user;
   res.cookie('jwt', token, { httpOnly: true });
   console.log(req.user);
   // res.status(200).json({ success: true, message: 'Login successful', token, data: req.user });
-  res.redirect(`http://localhost:5173/${role == 'customer' ? 'users/profile/me' : 'barbers/profile/me'}?token=${token}&method=googleoauth`); // Redirect to dashboard after login
+  res.redirect(`${frontend_URL}/${role == 'customer' ? 'users/profile/me' : 'barbers/profile/me'}?token=${token}&method=googleoauth`); // Redirect to dashboard after login
 });
 
 export default loginRouter
